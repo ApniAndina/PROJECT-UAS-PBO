3272c0a7923a84bab853251b204ef15470877518
ApniAndina <96295308+ApniAndina@users.noreply.github.com>
2021-12-27 08:27:13 +0700

    Update dbHelper.java

--- UAS-Project-PBO-main/UAS-Project-PBO-main/src/database/dbHelper.java
+++ UAS-Project-PBO-main/UAS-Project-PBO-main/src/database/dbHelper.java
@@ -8,7 +8,7 @@ public class dbHelper {
     public Connection getConnection(){
         try{
             Class.forName("org.sqlite.JDBC");
-            con = DriverManager.getConnection("jdbc:sqlite:src\\database\\Koperasi.db");
+            con = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Apni\\SqliteDb\\dbkoperasi.sqlite");
             System.out.println("Success!");
             
             return con;
@@ -28,10 +28,10 @@ public class dbHelper {
         try{
             // Create table nasabah
             query = "CREATE TABLE IF NOT EXISTS nasabah (\n" +
-                       "    id      INTEGER       PRIMARY KEY\n" +
+                       "    id     INT (10)      PRIMARY KEY\n" +
                        "                          NOT NULL,\n" +
-                       "    name    VARCHAR (256) NOT NULL,\n" +
-                       "    address VARCHAR (256) NOT NULL\n" +
+                       "    name    VARCHAR (50) NOT NULL,\n" +
+                       "    address VARCHAR (100) NOT NULL\n" +
                        ");";
             
             state = con.prepareStatement(query);
@@ -39,11 +39,11 @@ public class dbHelper {
             
             // Create table individu
             query = "CREATE TABLE IF NOT EXISTS individu (\n" +
-                    "    id                      PRIMARY KEY\n" +
+                    "    id      INT (10)               PRIMARY KEY\n" +
                     "                            REFERENCES nasabah (id) ON DELETE RESTRICT\n" +
                     "                                                    ON UPDATE CASCADE,\n" +
-                    "    nik  LONG NOT NULL,\n" +
-                    "    npwp LONG NOT NULL\n" +
+                    "    nik  INT (16) NOT NULL,\n" +
+                    "    npwp INT (15) NOT NULL\n" +
                     ");";
             
             state = con.prepareStatement(query);
@@ -51,10 +51,10 @@ public class dbHelper {
             
             // Create table perusahaan
             query = "CREATE TABLE IF NOT EXISTS perusahaan (\n" +
-                    "    id                    PRIMARY KEY\n" +
+                    "    id    INT (10)              PRIMARY KEY\n" +
                     "                          REFERENCES nasabah (id) ON DELETE RESTRICT\n" +
                     "                                                  ON UPDATE CASCADE,\n" +
-                    "    nib VARCHAR (256) NOT NULL\n" +
+                    "    nib VARCHAR (13) NOT NULL\n" +
                     ");";
             
             state = con.prepareStatement(query);
@@ -62,10 +62,10 @@ public class dbHelper {
             
             // Create table rekening
             query = "CREATE TABLE IF NOT EXISTS rekening (\n" +
-                    "    no_rekening INTEGER PRIMARY KEY\n" +
+                    "    no_rekening INT (10) PRIMARY KEY\n" +
                     "                        NOT NULL,\n" +
-                    "    saldo       DOUBLE  NOT NULL,\n" +
-                    "    id                  REFERENCES nasabah (id) ON DELETE RESTRICT\n" +
+                    "    Saldo       DOUBLE (16,2)  NOT NULL,\n" +
+                    "    id  INT (10)    REFERENCES nasabah (id) ON DELETE RESTRICT\n" +
                     "                                                ON UPDATE CASCADE\n" +
                     ");";
             
