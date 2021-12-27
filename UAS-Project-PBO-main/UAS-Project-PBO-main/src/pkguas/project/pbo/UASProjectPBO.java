3d6ad40a6c8e75309444f699d1ab2a1fb2dd688e
ApniAndina <96295308+ApniAndina@users.noreply.github.com>
2021-12-27 08:29:38 +0700

    Update UASProjectPBO.java

--- UAS-Project-PBO-main/UAS-Project-PBO-main/src/pkguas/project/pbo/UASProjectPBO.java
+++ UAS-Project-PBO-main/UAS-Project-PBO-main/src/pkguas/project/pbo/UASProjectPBO.java
@@ -8,7 +8,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import koperasi.*;
 
-public class FinalProjectPBO extends Application {
+public class UASProjectPBO extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
